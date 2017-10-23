518f34b85a57b3695798076be906cf77c6596f00
KLIANS <365685949@qq.com>
2017-10-23 17:33:54 +0800

    11

---
+++ GLAlertView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GLAlertView
+//
+//  Created by KLIANS on 2017/10/23.
+//  Copyright © 2017年 KLIANS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GLAlertView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GLAlertView
+//
+//  Created by KLIANS on 2017/10/23.
+//  Copyright © 2017年 KLIANS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
